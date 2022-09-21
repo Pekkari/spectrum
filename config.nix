4a9903ea3e190e34cc374945b0e4ca750987ce2a
José Pekkarinen <jose.pekkarinen@unikie.com>
2022-09-21 09:15:41 +0300

    Add x86_64 linux hardened overlay
    Signed-off-by: José Pekkarinen <jose.pekkarinen@unikie.com>

---
+++ config.nix
@@ -0,0 +1,27 @@
+{
+  pkgs = import <nixpkgs> {
+    overlays = [
+ 
+      (final: super: {
+        makeModulesClosure = args: super.makeModulesClosure (args // {
+          rootModules = [];
+        });
+      })
+
+      (final: super: {
+        linux_latest = super.pkgs.linuxKernel.manualConfig {
+          inherit (super.pkgs) stdenv lib;
+          version = "5.15.53-hardened1";
+       
+          src = super.pkgs.fetchurl {
+            url = "https://github.com/anthraxx/linux-hardened/archive/refs/tags/5.15.53-hardened1.tar.gz";
+            sha256 = "3157a4cadd167e0892cd4257f778dbe8d00b7ff6300c9c2dafaabb13852fa2fc";
+          };
+       
+          configfile = ../host/rootfs/generated_x86_qemu-nixos_host_release_defconfig;
+          allowImportFromDerivation = true;
+        };
+      })
+    ];
+  };
+}
